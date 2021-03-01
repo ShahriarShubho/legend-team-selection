64fe50d1dceedb185b9b9608299c616dacd1b563
Shahriar Shubho <you@example.com>
2021-03-01 20:39:20 +0600

    assignment complete

--- src/components/SelectedClub/SelectedClub.js
+++ src/components/SelectedClub/SelectedClub.js
@@ -3,16 +3,26 @@ import ShowSelectedPlayerData from '../ShowSelectedPlayerData/ShowSelectedPlayer
 import './SelectedClub.css'
 const SelectedClub = (props) => {
     const players = props.playerInfo
-    const totalSalary = players.reduce((sum, player) => sum + player.salary, 0);
+
+    //This code for remove duplicate player for team
+        let uniquePlayers = [];
+        for (let i = 0; i < players.length; i++) {
+            const player = players[i];
+            const index = uniquePlayers.indexOf(player);
+            if(index ===-1){
+                uniquePlayers.push(player)
+            }         
+        }
+
+    const totalSalary = uniquePlayers.reduce((sum, player) => sum + player.salary, 0);
 
     return (
         <div className="selected-player-div">
             <h1>Selected Team</h1>
-            <h3>Player Selected : {players.length}</h3>
+            <h3>Player Selected : {uniquePlayers.length}</h3>
             <h5>Total Budge : ${totalSalary}</h5> <br/>
             <h4>Selected Player List :</h4>
-            {players.map(showPlayer =><ShowSelectedPlayerData showPlayerData={showPlayer} key={showPlayer.id}></ShowSelectedPlayerData> )}
-            
+            {uniquePlayers.map(showPlayer =><ShowSelectedPlayerData showPlayerData={showPlayer} key={showPlayer.id}></ShowSelectedPlayerData> )}           
         </div>
     );
 };
